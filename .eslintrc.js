a2901805409bb415f22460625930563632ae2e7b
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-02 23:51:59 +0700

    feat: :rocket: initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'next',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+
+    // Sort
+    'simple-import-sort/exports': 'warn',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          // ext library & side effect imports
+          ['^@?\\w', '^\\u0000'],
+          // {s}css files
+          ['^.+\\.s?css$'],
+          // app
+          ['^app'],
+          // components
+          ['^components'],
+          // constants
+          ['^constants'],
+          // layouts
+          ['^layouts'],
+          // models
+          ['^models'],
+          // pages
+          ['^pages'],
+          // services
+          ['^services'],
+          // styles
+          ['^styles'],
+          // utils
+          ['^utils'],
+          // relative paths up until 3 level
+          [
+            '^\\./?$',
+            '^\\.(?!/?$)',
+            '^\\.\\./?$',
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./\\.\\./?$',
+            '^\\.\\./\\.\\.(?!/?$)',
+            '^\\.\\./\\.\\./\\.\\./?$',
+            '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+          ],
+          ['^@/types'],
+          // other that didnt fit in
+          ['^'],
+        ],
+      },
+    ],
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+};
