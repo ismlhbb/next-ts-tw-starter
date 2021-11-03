8794b4ce08783f4e58bce87c398abe70c394da85
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-03 16:14:33 +0700

    fix: change `AppState` to `RootState`

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -3,7 +3,7 @@ import type { ChangeEvent } from 'react';
 import { useEffect, useRef } from 'react';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
-import type { AppDispatch, AppState } from './store';
+import type { AppDispatch, RootState } from './store';
 
 export const useForm =
   <TContent>(defaultValues: TContent) =>
@@ -45,4 +45,4 @@ export const useInterval = (callback: Function, delay: number) => {
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
