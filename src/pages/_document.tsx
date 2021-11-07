687f3e4ec10f10a4498f22d57fe9ebd42e8d7c91
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-07 21:58:54 +0700

    feat: update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,13 +46,30 @@ class MyDocument extends Document {
           <meta itemProp='name' content={defaultMeta.title} />
           <meta itemProp='description' content={defaultMeta.description} />
           {/* Favicons */}
-          {/* <link
+          <link
             rel='apple-touch-icon'
             sizes='180x180'
-            href='/favicons/apple-touch-icon.png'
+            href='/favicon/apple-touch-icon.png'
           />
-          <link rel='shortcut icon' href='/favicons/app-icon.png' /> */}
-          <link rel='manifest' href='/manifest.json' />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon/favicon-16x16.png'
+          />
+          <link
+            rel='mask-icon'
+            href='/favicon/safari-pinned-tab.svg'
+            color='#000000'
+          />
+          <link rel='shortcut icon' href='/favicon/favicon.ico' />
+          <link rel='manifest' href='/favicon/manifest.json' />
         </Head>
         <body>
           <Main />
