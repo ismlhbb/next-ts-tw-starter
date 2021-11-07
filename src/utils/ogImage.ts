6d00a953d5a01e9fce2cab76d469ab4b7e526f5d
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-07 13:43:39 +0700

    fix: og image url

--- src/utils/ogImage.ts
+++ src/utils/ogImage.ts
@@ -9,7 +9,7 @@ export const ogImage = (text: string, options?: OgImageOptions) => {
   const defaultOptions: OgImageOptions = {
     theme: 'dark',
     fontSize: '100px',
-    images: 'https://next-ts-tw-starter.vercel.app/nextjs-black-logo.svg',
+    images: 'https://next-ts-tw-starter.vercel.app/nextjs-white-logo.svg',
     images2: 'https://next-ts-tw-starter.vercel.app/ts.svg',
   };
 
@@ -18,8 +18,10 @@ export const ogImage = (text: string, options?: OgImageOptions) => {
     ...options,
   };
 
-  if (finalOptions.images2) {
+  // eslint-disable-next-line no-extra-boolean-cast
+  if (!!finalOptions.images2) {
     return `https://og-image-ismlhbb.vercel.app/ismlhbb%2F**${text}**.png?theme=${finalOptions.theme}&md=1&fontSize=${finalOptions.fontSize}&images=${finalOptions.images}&images=${finalOptions.images2}`;
+  } else {
+    return `https://og-image-ismlhbb.vercel.app/ismlhbb%2F**${text}**.png?theme=${finalOptions.theme}&md=1&fontSize=${finalOptions.fontSize}&images=${finalOptions.images}`;
   }
-  return `https://og-image-ismlhbb.vercel.app/ismlhbb%2F**${text}**.png?theme=${finalOptions.theme}&md=1&fontSize=${finalOptions.fontSize}&images=${finalOptions.images}`;
 };
