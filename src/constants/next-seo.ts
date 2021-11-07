14b7fe40970c7dd6db1472eebd21390810ad2f3c
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-07 12:51:48 +0700

    fix: `defaultSeoConfig` og image

--- src/constants/next-seo.ts
+++ src/constants/next-seo.ts
@@ -1,7 +1,5 @@
 import { DefaultSeoProps } from 'next-seo';
 
-import { ogImage } from 'utils/ogImage';
-
 export const defaultMeta = {
   title: process.env.NEXT_PUBLIC_APPLICATION_NAME,
   siteName: process.env.NEXT_PUBLIC_APPLICATION_NAME,
@@ -12,12 +10,13 @@ export const defaultMeta = {
   keywords:
     'next, react, tailwindcss, redux, typescript, javascript, starter, template, boilerplate',
   url: 'https://next-ts-tw-starter.vercel.app',
-  image: '',
+  image:
+    'https://og-image-ismlhbb.vercel.app/ismlhbb%2F**next-ts-tw-starter**.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg&images=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fremojansen%2Flogo.ts%40master%2Fts.svg',
 };
 
 export const defaultSEOConfig: DefaultSeoProps = {
   title: defaultMeta.title,
-  titleTemplate: `%s | ${defaultMeta.title}`,
+  titleTemplate: `${defaultMeta.title} | %s`,
   defaultTitle: defaultMeta.title,
   description: defaultMeta.description,
   canonical: defaultMeta.url,
@@ -27,7 +26,7 @@ export const defaultSEOConfig: DefaultSeoProps = {
     description: defaultMeta.description,
     images: [
       {
-        url: ogImage('next-ts-tw-starter'),
+        url: defaultMeta.image,
         alt: 'ismlhbb/next-ts-tw-starter og-image',
       },
     ],
