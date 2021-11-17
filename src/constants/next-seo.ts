bc4ba7213a352d09de3fd4f1254d4d655fca26e5
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-17 20:34:21 +0700

    refactor(seo): move additional meta and link to `next-seo`

--- src/constants/next-seo.ts
+++ src/constants/next-seo.ts
@@ -11,8 +11,9 @@ export const defaultMeta = {
   robots: 'follow, index',
   keywords:
     'next, react, tailwindcss, redux, typescript, javascript, starter, template, boilerplate',
-  url: 'https://next-ts-tw-starter.vercel.app',
+  url: process.env.NEXT_PUBLIC_APPLICATION_BASE_URL,
   image: ogImage('next-ts-tw-starter'),
+  themeColor: '#EE424D',
 };
 
 export const defaultSEOConfig: DefaultSeoProps = {
@@ -38,4 +39,74 @@ export const defaultSEOConfig: DefaultSeoProps = {
     handle: '@ismlhbb',
     cardType: 'summary_large_image',
   },
+  additionalMetaTags: [
+    {
+      name: 'viewport',
+      content: 'initial-scale=1.0, width=device-width',
+    },
+    {
+      name: 'format-detection',
+      content: 'telephone=no',
+    },
+    {
+      name: 'mobile-web-app-capable',
+      content: 'yes',
+    },
+    {
+      name: 'theme-color',
+      content: defaultMeta.themeColor,
+    },
+    {
+      name: 'keyword',
+      content: defaultMeta.keywords,
+    },
+    {
+      name: 'apple-mobile-web-app-capable',
+      content: 'yes',
+    },
+    {
+      name: 'apple-mobile-web-app-title',
+      content: defaultMeta.title,
+    },
+    {
+      name: 'apple-mobile-web-app-status-bar-style',
+      content: 'default',
+    },
+    {
+      name: 'msapplication-TileColor',
+      content: defaultMeta.themeColor,
+    },
+  ],
+  additionalLinkTags: [
+    {
+      rel: 'apple-touch-icon',
+      href: '/favicon/apple-touch-icon.png',
+      sizes: '180x180',
+    },
+    {
+      rel: 'icon',
+      href: '/favicon/favicon-32x32.png',
+      sizes: '32x32',
+      type: 'image/png',
+    },
+    {
+      rel: 'icon',
+      href: '/favicon/favicon-16x16.png',
+      sizes: '16x16',
+      type: 'image/png',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/favicon/safari-pinned-tab.svg',
+      color: '#000000',
+    },
+    {
+      rel: 'shortcut icon',
+      href: '/favicon/favicon.ico',
+    },
+    {
+      rel: 'manifest',
+      href: '/favicon/manifest.json',
+    },
+  ],
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,6 @@ import Document, {
   NextScript,
 } from 'next/document';
 
-import { defaultMeta } from 'constants/next-seo';
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const originalRenderPage = ctx.renderPage;
@@ -28,49 +26,8 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='id'>
-        <Head>
-          <meta name='application-name' content={defaultMeta.title} />
-          <meta name='format-detection' content='telephone=no' />
-          <meta name='mobile-web-app-capable' content='yes' />
-          <meta name='theme-color' content='#EE424D' />
-          <meta name='keyword' content={defaultMeta.keywords} />
-          {/* Apple */}
-          <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-title' content={defaultMeta.title} />
-          <meta
-            name='apple-mobile-web-app-status-bar-style'
-            content='default'
-          />
-          {/* ItemProp */}
-          <meta itemProp='name' content={defaultMeta.title} />
-          <meta itemProp='description' content={defaultMeta.description} />
-          {/* Favicons */}
-          <link
-            rel='apple-touch-icon'
-            sizes='180x180'
-            href='/favicon/apple-touch-icon.png'
-          />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='32x32'
-            href='/favicon/favicon-32x32.png'
-          />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='16x16'
-            href='/favicon/favicon-16x16.png'
-          />
-          <link
-            rel='mask-icon'
-            href='/favicon/safari-pinned-tab.svg'
-            color='#000000'
-          />
-          <link rel='shortcut icon' href='/favicon/favicon.ico' />
-          <link rel='manifest' href='/favicon/manifest.json' />
-        </Head>
+      <Html lang='en' prefix='og: https://ogp.me/ns#'>
+        <Head />
         <body>
           <Main />
           <NextScript />
